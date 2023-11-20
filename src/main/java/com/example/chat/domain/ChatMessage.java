db6c9d32adbb68fe1c17cf67de0c37b518affd5d
suddiyo <suddiyo@naver.com>
2023-11-20 19:47:18 +0900

    Feat: ChatMessage, ChatRoom Entity 구현

---
+++ src/main/java/com/example/chat/domain/ChatMessage.java
@@ -0,0 +1,34 @@
+package com.example.chat.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatRoom_Id")
+    private ChatRoom chatRoom;
+
+    private Long senderId;
+    private String message;
+
+}
---
+++ src/main/java/com/example/chat/domain/ChatRoom.java
@@ -0,0 +1,33 @@
+package com.example.chat.domain;
+
+import static lombok.AccessLevel.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatMessage> chatMessages;
+
+}
