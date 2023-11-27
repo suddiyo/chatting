32c354e87b53311d94e4dcc55d35691fcb6e0c8d
suddiyo <suddiyo@naver.com>
2023-11-27 22:01:55 +0900

    [Feat/#3] ChatMessage, ChatRoom 작성

--- rabbitmq/src/main/java/com/example/rabbitmq/dto/ChatMessage.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/dto/ChatMessage.java
@@ -1,2 +1,21 @@
-package com.example.rabbitmq.dto;public class ChatMessage {
+package com.example.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
 }
--- rabbitmq/src/main/java/com/example/rabbitmq/dto/ChatRoom.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/dto/ChatRoom.java
@@ -1,2 +1,23 @@
-package com.example.rabbitmq.dto;public class ChatRoom {
+package com.example.rabbitmq.dto;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatRoom {
+
+    private String roomId;
+    private String roomName;
+
+
+    public static ChatRoom create(String name) {
+        ChatRoom room = new ChatRoom();
+        room.roomId = UUID.randomUUID().toString();
+        room.roomName = name;
+        return room;
+    }
 }
