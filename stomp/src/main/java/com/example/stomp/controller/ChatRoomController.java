257717f3c4a523bac810b4801e9e6541d6b3c44d
ahnsugyeong <88311377+ahnsugyeong@users.noreply.github.com>
2023-11-27 20:31:48 +0900

    [Feat/#1] WebSocket + STOMP로 채팅 구현 (#2)
    * [Feat/#1] 프로젝트 초기 설정
    
    * [Feat/#1] DB 연동 및 환경 설정
    
    * [Feat/#1] ChatMessage, ChatRoom DTO 생성 및 ChatConfig 설정
    
    * [Feat/#1] ChatRoomService 구현
    
    * [Feat/#1] ChatMessageController, ChatRoomController 구현
    
    * [Feat/#1] View Template 구현

---
+++ stomp/src/main/java/com/example/stomp/controller/ChatRoomController.java
@@ -0,0 +1,52 @@
+package com.example.stomp.controller;
+
+import com.example.stomp.dto.ChatRoom;
+import com.example.stomp.service.ChatRoomService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+@Controller
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping("/room")
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomService.findAllRoom();
+    }
+
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomService.createRoom(name);
+    }
+
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomService.findById(roomId);
+    }
+
+}
---
+++ stomp/src/main/java/com/example/stomp/service/ChatRoomService.java
@@ -0,0 +1,41 @@
+package com.example.stomp.service;
+
+import com.example.stomp.dto.ChatRoom;
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        List<ChatRoom> result = new ArrayList<>(chatRooms.values());
+        Collections.reverse(result);
+        return result;
+    }
+
+    public ChatRoom findById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRooms.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
